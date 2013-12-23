930efb9fb4538a96c1082c7d6f2239dd58febe7a
larstvei <larstvei@student.matnat.uio.no>
2013-12-23 02:52:18 +0100

    Rewrote my Emacs configurations as a org-file.

--- init.el
+++ init.el
@@ -1,186 +1,234 @@
-;;-----------------------------------------------------------------------;;
-;;----------------------[ init.el by Lars Tveito ]-----------------------;;
-;;---------------------------[ 15. Jul, 2012 ]---------------------------;;
-;;-----------------------------------------------------------------------;;
 
+(defun init-hook ()
+  "If the current buffer is 'init.org' the code-blocks are
+tangled, and the tangled file is compiled."
+  (when (equal (buffer-file-name)
+               (concat user-emacs-directory "init.org"))
+    (org-babel-tangle)
+    (byte-compile-file (concat user-emacs-directory "init.el"))))
 
-;------------------------------[ libraries ]------------------------------;
-
-(let ((default-directory "~/Dropbox/ifi/.emacs.d/site-lisp/"))
-  (normal-top-level-add-to-load-path '("."))
-  (normal-top-level-add-subdirs-to-load-path))
-
-;-------------------------------------------------------------------------;
-
-
-
-;---------------------------[ initializations ]---------------------------;
+(add-hook 'after-save-hook 'init-hook)
 
 (package-initialize)
-(color-theme-initialize)
 
-;-------------------------------------------------------------------------;
+(add-to-list 'package-archives
+             '("MELPA" . "http://melpa.milkbox.net/packages/") t)
 
+(let ((packages
+       '(ac-geiser         ; Auto-complete backend for geiser
+         ac-slime          ; An auto-complete source using slime completions
+         ace-jump-mode     ; quick cursor location minor mode
+         auto-compile      ; automatically compile Emacs Lisp libraries
+         auto-complete     ; auto completion
+         elscreen          ; window session manager
+         expand-region     ; Increase selected region by semantic units
+         flx-ido           ; flx integration for ido
+         ido-vertical-mode ; Makes ido-mode display vertically.
+         geiser            ; GNU Emacs and Scheme talk to each other
+         haskell-mode      ; A Haskell editing mode
+         jedi              ; Python auto-completion for Emacs
+         magit             ; control Git from Emacs
+         markdown-mode     ; Emacs Major mode for Markdown-formatted files.
+         monokai-theme     ; A fruity color theme for Emacs.
+         move-text         ; Move current line or region with M-up or M-down
+         multiple-cursors  ; Multiple cursors for Emacs.
+         org               ; Outline-based notes management and organizer
+         paredit           ; minor mode for editing parentheses
+         pretty-lambdada   ; the word `lambda' as the Greek letter.
+         ;; slime          ; Superior Lisp Interaction Mode for Emacs
+         smex              ; M-x interface with Ido-style fuzzy matching.
+         )))
+  ;; 'remove-if' is a part of the cl-library, so we require this feature.
+  (require 'cl)
+  ;; Filter out installed packages and install the remaining.
+  (mapc 'package-install (remove-if 'package-installed-p packages)))
 
+(dolist (feature
+         '(auto-compile             ; auto-compile .el files
+           auto-complete-config     ; a configuration for auto-complete-mode
+           jedi                     ; auto-completion for python
+           pretty-lambdada          ; show 'lambda' as the greek letter.
+           ox-latex                 ; the latex-exporter (from org)
+           recentf                  ; recently opened files
+           tex-mode                 ; TeX, LaTeX, and SliTeX mode commands
+           ))
+  (require feature))
 
-;--------------------------[ loads color-theme ]--------------------------;
+(setq initial-scratch-message nil     ; Clean scratch buffer.
+      inhibit-startup-message t       ; No splash screen please.
+      default-input-method "TeX"      ; Use TeX when toggeling input method.
+      doc-view-continuous t           ; At page edge goto next/previous.
+      echo-keystrokes 0.1             ; Show keystrokes asap.
+      )
 
-(load-library "subdued")
-(require 'color-theme-subdued)
-(color-theme-subdued)
+(setq-default fill-column 76                   ; Maximum line width.
+              indent-tabs-mode nil             ; Use spaces instead of tabs.
+              split-width-threshold 100        ; Split verticly by default.
+              auto-fill-function 'do-auto-fill ; Auto-fill-mode everywhere.
+              )
 
-;-------------------------------------------------------------------------;
-
-
-
-;--------------------[ remove bars, blink and splash ]--------------------;
-
-(menu-bar-mode                  -1)
-(tool-bar-mode                  -1)
-(scroll-bar-mode                -1)
-(blink-cursor-mode               0)
-(setq inhibit-startup-message    t)
-(setq-default indent-tabs-mode nil)
-
-;-------------------------------------------------------------------------;
-
-
-
-;-----------------------------[ Für Package ]-----------------------------;
-
-(setq package-archives 
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-	("marmalade" . "http://marmalade-repo.org/packages/")
-	("melpa" . "http://melpa.milkbox.net/packages/")))
-
-;-------------------------------------------------------------------------;
-
-
-
-;----------------------[ UTF-8 and tex-input mode ]-----------------------;
-
-(set-language-environment "UTF-8")
-(setq default-input-method "TeX"
-      initial-scratch-message '())
-
-;-------------------------------------------------------------------------;
-
-
-
-;-----------------[ show parentheses, ido-mode and y/n ]------------------;
-
-(ido-mode t)
-(show-paren-mode t)
 (fset 'yes-or-no-p 'y-or-n-p)
-(setq ido-file-extensions-order 
-      '("src" ".Org" ".java" ".c" ".h" ".emacs" ".el" ".tex"))
 
-;-------------------------------------------------------------------------;
+(defvar emacs-autosave-directory
+  (concat user-emacs-directory "autosaves/")
+  "This variable dictates where to put auto saves. It is set to a
+  directory called autosaves located wherever your .emacs.d/ is
+  located.")
 
-
-
-;-------------[ Delete selection, veiw column, load header ]--------------;
-
-(delete-selection-mode t)
-(setq column-number-mode t)
-(autoload 'header "header" nil t)
-
-;-------------------------------------------------------------------------;
-
-
-
-;--------------------------------[ Mail ]---------------------------------;
-
-(setq
- user-mail-address             "larstvei@student.matnat.uio.no"
- user-full-name                "Lars Tveito"
- send-mail-function            'smtpmail-send-it
- smtpmail-smtp-server          "smtp.uio.no"
- smtpmail-smtp-service         587)
-
-;-------------------------------------------------------------------------;
-
-
-
-;------------------------------[ Autosave ]-------------------------------;
-
-(defvar emacs-autosave-directory "~/Dropbox/ifi/.emacs.d/autosaves/")
+;; Sets all files to be backed up and auto saved in a single directory.
 (setq backup-directory-alist
       `((".*" . ,emacs-autosave-directory))
       auto-save-file-name-transforms
       `((".*" ,emacs-autosave-directory t)))
-      
-;-------------------------------------------------------------------------;
 
+(set-language-environment "UTF-8")
 
+(put 'narrow-to-region 'disabled nil)
 
-;---------------------------[ Resize buffers ]----------------------------;
-
-(global-set-key (kbd "<M-left>") 
-		(lambda () (interactive)
-                  (enlarge-window -1 t)))
-(global-set-key (kbd "<M-right>") 
-		(lambda () (interactive)
-                  (enlarge-window 1 t)))
-(global-set-key (kbd "<M-up>") 
-		(lambda () (interactive)
-                  (enlarge-window -1)))
-(global-set-key (kbd "<M-down>") 
-		(lambda () (interactive)
-                  (enlarge-window 1)))
-
-;-------------------------------------------------------------------------;
-
-
-
-;--------------------[ start eshell and bind to key ]---------------------;
-
-(global-set-key (kbd "C-x t") 
-		(lambda () (interactive) 
-		  (if (string= (buffer-name) "*eshell*")
-		      (switch-to-buffer (second (buffer-list)))
-		      (eshell))))
-
-;-------------------------------------------------------------------------;
-
-
-
-;----------------------------[ Auto-complete ]----------------------------;
-
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories 
-	     "/home/lars/Dropbox/ifi/.emacs.d/lisp//ac-dict")
 (ac-config-default)
 
-;-------------------------------------------------------------------------;
-
-
-;-------------------------------[ DocView ]-------------------------------;
-
 (add-hook 'doc-view-mode-hook 'auto-revert-mode)
-(setq doc-view-continuous t)
 
-;-------------------------------------------------------------------------;
+(dolist (mode
+         '(tool-bar-mode                ; No toolbars, more room for text.
+           scroll-bar-mode              ; No scroll bars either.
+           blink-cursor-mode            ; The blinking cursor gets old.
+           ))
+  (funcall mode 0))
 
+(dolist (mode
+         '(abbrev-mode                ; E.g. sopl -> System.out.println.
+           auto-compile-on-load-mode  ; Compile .el files on load ...
+           auto-compile-on-save-mode  ; ... and save.
+           column-number-mode         ; Show column number in mode line.
+           delete-selection-mode      ; Replace selected text.
+           recentf-mode               ; Recently opened files.
+           show-paren-mode            ; Highlight matching parentheses.
+           ))
+  (funcall mode 1))
 
-;----------------------------[ Key bindings ]-----------------------------;
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-(global-set-key (kbd "C-x k") 'kill-this-buffer)
+(load-theme 'monokai t)
 
-(key-chord-mode 1)
+(when (member "Inconsolata" (font-family-list))
+  (set-face-attribute 'default nil :font "Inconsolata-13"))
 
-(key-chord-define-global ";'" 'ace-jump-mode)
-(key-chord-define-global "qw" 'mc/mark-all-like-this)
-(key-chord-define-global "qs" 'mc/mark-next-like-this)
+(dolist (mode
+         '(ido-mode                   ; Interactivly do.
+           ido-everywhere             ; Use Ido for all buffer/file reading.
+           ido-vertical-mode          ; Makes ido-mode display vertically.
+           flx-ido-mode               ; Toggle flx ido mode.
+           ))
+  (funcall mode 1))
 
-(global-set-key (kbd "C-'") 'er/expand-region)
-(global-set-key (kbd "C-;") 'er/contract-region)
+(setq ido-file-extensions-order
+      '(".el" ".scm" ".lisp" ".java" ".c" ".h" ".org" ".tex"))
 
-;-------------------------------------------------------------------------;
+(add-to-list 'ido-ignore-buffers "*Messages*")
 
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
 
+(setq user-full-name         "Lars Tveito"
+      user-mail-address      "larstvei@ifi.uio.no"
+      smtpmail-smtp-server   "smtp.uio.no"
+      smtpmail-smtp-service   587)
 
-;-------------------[ Replace expression with output ]--------------------
+(defun calendar-show-week (arg)
+  "Displaying week number in calendar-mode."
+  (interactive "P")
+  (copy-face font-lock-constant-face 'calendar-iso-week-face)
+  (set-face-attribute
+   'calendar-iso-week-face nil :height 0.7)
+  (setq calendar-intermonth-text
+        (and arg
+             '(propertize
+               (format
+                "%2d"
+                (car (calendar-iso-from-absolute
+                      (calendar-absolute-from-gregorian
+                       (list month day year)))))
+               'font-lock-face 'calendar-iso-week-face))))
+
+(calendar-show-week t)
+
+(setq calendar-week-start-day 1
+      calendar-latitude 60.0
+      calendar-longitude 10.7
+      calendar-location-name "Oslo, Norway")
+
+(add-hook 'text-mode-hook 'turn-on-flyspell)
+
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
+(ac-flyspell-workaround)
+
+(setq org-agenda-start-on-weekday nil             ; Show agenda from today.
+      org-agenda-files '("~/Dropbox/life.org")    ; A list of agenda files.
+      org-agenda-default-appointment-duration 120 ; 2 hours appointments.
+      )
+
+(setq org-src-fontify-natively t)
+
+(defun recentf-ido-find-file ()
+  "Find a recent file using Ido."
+  (interactive)
+  (let ((f (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when f
+      (find-file f))))
+
+(defun remove-whitespace-inbetween ()
+  "Removes whitespace before and after the point."
+  (interactive)
+  (just-one-space -1))
+
+(defun switch-to-shell ()
+  "Jumps to eshell or back."
+  (interactive)
+  (if (string= (buffer-name) "*shell*")
+      (switch-to-prev-buffer)
+    (shell)))
+
+(defun duplicate-thing ()
+  "Ethier duplicates the line or the region"
+  (interactive)
+  (save-excursion
+    (let ((start (if (region-active-p) (region-beginning) (point-at-bol)))
+          (end   (if (region-active-p) (region-end) (point-at-eol))))
+      (goto-char end)
+      (unless (region-active-p)
+        (newline))
+      (insert (buffer-substring start end)))))
+
+(defun tidy ()
+  "Ident, untabify and unwhitespacify current buffer, or region if active."
+  (interactive)
+  (let ((beg (if (region-active-p) (region-beginning) (point-min)))
+        (end (if (region-active-p) (region-end) (point-max))))
+    (indent-region beg end)
+    (whitespace-cleanup)
+    (untabify beg (if (< end (point-max)) end (point-max)))))
+
+(global-set-key (kbd "C-'")  'er/expand-region)
+(global-set-key (kbd "C-;")  'er/contract-region)
+
+(global-set-key (kbd "C-c e")  'mc/edit-lines)
+(global-set-key (kbd "C-c a")  'mc/mark-all-like-this)
+(global-set-key (kbd "C-c n")  'mc/mark-next-like-this)
+
+(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+
+(global-set-key (kbd "C-c t")    'org-agenda-list)
+(global-set-key (kbd "C-x k")    'kill-this-buffer)
+(global-set-key (kbd "C-x C-r")  'recentf-ido-find-file)
+
+(global-set-key (kbd "C-c j")    'remove-whitespace-inbetween)
+(global-set-key (kbd "C-x t")    'switch-to-shell)
+(global-set-key (kbd "C-c d")    'duplicate-thing)
+(global-set-key (kbd "<C-tab>")  'tidy)
+
+(global-set-key (kbd "<M-S-up>")    'move-text-up)
+(global-set-key (kbd "<M-S-down>")  'move-text-down)
 
 (defadvice eval-last-sexp (around replace-sexp (arg) activate)
   "Replace sexp when called with a prefix argument."
@@ -192,71 +240,81 @@
         (forward-sexp))
     ad-do-it))
 
-;-------------------------------------------------------------------------;
+(dolist (mode '(slime-repl-mode inferior-lisp-mode inferior-scheme-mode))
+  (add-to-list 'pretty-lambda-auto-modes mode))
 
+(pretty-lambda-for-modes)
 
-;-------------------------------------------------------------------------;
-;--------------[ * Programming-language-modes specifics * ]---------------;
-;-------------------------------------------------------------------------;
+(dolist (mode pretty-lambda-auto-modes)
+  ;; add paredit-mode to all mode-hooks
+  (add-hook (intern (concat (symbol-name mode) "-hook")) 'paredit-mode))
 
-;--------------------------------[ Java ]---------------------------------;
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
 
-(autoload 'java-extras "java-extras" "Fold brackets")
-(add-hook 'java-mode-hook 'java-extras)
-;;TODO Fix this ugliness
-
-;-------------------------------------------------------------------------;
-
-
-;----------------------------------[ C ]----------------------------------;
-
-(load-library "hideshow-on")
-(add-hook 'c-mode-common-hook 'hideshow-on)
-
-;-------------------------------------------------------------------------;
-
-
-;--------------------------------[ Lisp ]---------------------------------;
-
-;; Show λ in Lisp code.
-(defun sm-lambda-mode-hook ()
-  (font-lock-add-keywords
-   nil `(("\\<lambda\\>"
-	  (0 (progn (compose-region (match-beginning 0) (match-end 0)
-				    ,(make-char 'greek-iso8859-7 107))
-		    nil))))))
-(dolist (h '(lisp-mode-hook
-             scheme-mode-hook
-             emacs-lisp-mode-hook
-             slime-repl-mode-hook
-             inferior-lisp-mode-hook
-             inferior-scheme-mode-hook
-             lisp-interaction-mode-hook))
-  (progn
-    (add-hook h (lambda () (paredit-mode 1)))
-    (add-hook h 'sm-lambda-mode-hook)))
-
-(when (file-exists-p "~/quicklisp/slime-helper.el")
-  (load (expand-file-name "~/quicklisp/slime-helper.el")))
+(when (file-exists-p "~/quicklisp/slime-helper.elc")
+  (load (expand-file-name "~/quicklisp/slime-helper.elc")))
 
 (setq inferior-lisp-program "sbcl")
 
-;-------------------------------------------------------------------------;
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
 
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'slime-repl-mode))
 
-;-------------------------------[ Scheme ]--------------------------------;
+(add-hook 'geiser-mode-hook 'ac-geiser-setup)
+(add-hook 'geiser-repl-mode-hook 'ac-geiser-setup)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'geiser-repl-mode))
 
-(require 'quack)
+(defun c-setup ()
+  (local-set-key (kbd "C-c C-c") 'compile))
 
-(autoload 'scheme-smart-complete "scheme-complete" nil t)
-(eval-after-load 'scheme
-  '(define-key scheme-mode-map "\t" 'scheme-complete-or-indent))
-(autoload 'scheme-get-current-symbol-info "scheme-complete" nil t)
-(add-hook 'scheme-mode-hook
-          (lambda ()
-            (make-local-variable 'eldoc-documentation-function)
-            (setq eldoc-documentation-function
-                  'scheme-get-current-symbol-info)
-            (eldoc-mode)))
+(add-hook 'c-mode-common-hook 'c-setup)
 
-;-------------------------------------------------------------------------;
+(define-abbrev-table 'java-mode-abbrev-table
+  '(("psv" "public static void main(String[] args) {" nil 0)
+    ("sopl" "System.out.println" nil 0)
+    ("sop" "System.out.printf" nil 0)))
+
+(defun java-setup ()
+  (abbrev-mode t)
+  (setq-local compile-command (concat "javac " (buffer-name))))
+
+(add-hook 'java-mode-hook 'java-setup)
+
+(defun asm-setup ()
+  (setq comment-start "#")
+  (local-set-key (kbd "C-c C-c") 'compile))
+
+(add-hook 'asm-mode-hook 'asm-setup)
+
+(add-to-list 'auto-mode-alist '("\\.tex\\'" . latex-mode))
+
+(setcar (cdr (cddaar tex-compile-commands)) " -shell-escape ")
+
+;; Add minted to the defaults packages to include when exporting.
+(add-to-list 'org-latex-packages-alist '("" "minted"))
+;; Tell the latex export to use the minted package for source
+;; code coloration.
+(setq org-latex-listings 'minted)
+;; Let the exporter use the -shell-escape option to let latex
+;; execute external programs.
+;; This obviously and can be dangerous to activate!
+(setq org-latex-pdf-process
+      (mapcar
+       (lambda (str)
+         (concat "pdflatex -shell-escape "
+                 (substring str (string-match "-" str))))
+       org-latex-pdf-process))
+
+(setq jedi:server-command
+      (cons "python3" (cdr jedi:server-command))
+      python-shell-interpreter "python3")
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+(add-hook 'python-mode-hook 'jedi:ac-setup)
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
